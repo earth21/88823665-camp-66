1fad0d23821da08b37cad2ad5e1dd45c98472c01
earth21 <earthaunnop.450@gmail.com>
2025-01-28 19:42:41 +0700

    errorUpdate

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MyController;
+use App\Http\Controllers\homecontroller;
+use App\Http\Controllers\logincontroller;
+use App\Http\Controllers\registercontroller;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,3 +19,28 @@ Route::get("/mylaravel/{id?}",
 
 Route::post("/mylaravel/{id?}", 
 [MyController::class,'myfunction']);
+
+
+Route::get("/home", 
+[homecontroller::class,'index']);
+
+Route::post("/home", 
+[homecontroller::class,'index']);
+
+Route::get("/", 
+[homecontroller::class,'index']);
+
+Route::post("/", 
+[homecontroller::class,'index']);
+
+Route::get("/login", 
+[logincontroller::class,'index']);
+
+Route::post("/login", 
+[logincontroller::class,'index']);
+
+Route::get("/register", 
+[registercontroller::class,'index']);
+
+Route::post("/register", 
+[registercontroller::class'index']);
