957e95a927cfd7ce133c11628c537b11852f6cfc
earth21 <earthaunnop.450@gmail.com>
2025-02-24 23:58:44 +0700

    updateProduct

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -6,6 +6,12 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
+use App\Http\Middleware\CheckLogin;
+
+
+Route::get("/product",[ProductController::class,"index"])->middleware([CheckLogin::class,]);
+Route::post("/product",[ProductController::class,"store"])->middleware([CheckLogin::class,]);
 
 Route::get('/mylaravel/{id?}',[Mycontroller::class,'myfunction']);
 Route::post('/mylaravel/{id?}',[Mycontroller::class,'myfunction']);
@@ -22,7 +28,7 @@ Route::post('/register',  [RegisterController::class,'create']);
 
 
 Route::get('/login',  [LoginController::class,'index']);
-Route::post('/login',  [LoginController::class,'index']);
+Route::post('/login',  [LoginController::class,'login']);
 
 
 
